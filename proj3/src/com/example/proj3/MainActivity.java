7acc9dddd90bc5ef445f4106ee7175894dc655b4
Brad Ichniowski <ichniow1@umbc.edu>
2013-05-09 15:15:46 -0400

    hello world

--- proj3/src/com/example/proj3/MainActivity.java
+++ proj3/src/com/example/proj3/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.proj3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        TextView text = new TextView(this);
+        text.setText("Hello World!");
+        setContentView(text);
     }
 
 
